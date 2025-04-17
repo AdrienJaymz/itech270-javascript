552c6c99adbce93e98a63864688002e98e8671ed
Adrien Wilson <awilson4541@stu.lltc.edu>
2025-04-17 10:12:17 -0500

    added a toggle box

--- event.js
+++ event.js
@@ -4,9 +4,20 @@ document.querySelector('#time-btn') .addEventListener('click', (e) =>{
     //e.preventDefault()
     console.log(e)
 })
+document.querySelector("#toggle-heading").addEventListener("click", toggleContent)
 }
 
 const getTheTime = () =>{
     const button = document.querySelector('#time-btn')
     console.log(button)
+}
+
+const toggleContent = (e) =>{
+    const content = document.querySelector('#toggle-content')
+    if(content.style.display != "block"){
+        content.style.display = "block";
+    }else{
+        content.style.display = "none";
+    }
+    console.log(content.style.display)
 }
